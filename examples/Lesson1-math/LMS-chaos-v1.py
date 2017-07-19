d4fa631e08681284a6e57973f68a83ccc6029afa
cangyeone <305601470@qq.com>
2017-07-19 20:55:32 +0800

    del some obsleting variable

--- examples/Lesson1-math/LMS-chaos-v1.py
+++ examples/Lesson1-math/LMS-chaos-v1.py
@@ -36,10 +36,6 @@ def update(num, data, lines, scate):
 
 mpl.style.use('fivethirtyeight')
 
-x = y = np.arange(-3.0, 3.0, 0.05)
-X, Y = np.meshgrid(x, y)
-zs = np.array([function(x,y) for x,y in zip(np.ravel(X), np.ravel(Y))])
-Z = zs.reshape(X.shape)
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
